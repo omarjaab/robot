84c93798aaea3b0c8c496ef378636926e700afdb
ojaab <omar.jaab@7c.is>
2025-03-05 03:37:29 +0100

    Added Room class to handle room boundaries

---
+++ src/room.js
@@ -0,0 +1,14 @@
+
+class Room {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    // Checks if the given coordinates are within the room boundaries
+    isWithinBounds(x, y) {
+        return x >= 0 && x < this.width && y >= 0 && y < this.height;
+    }
+}
+
+module.exports = Room;
